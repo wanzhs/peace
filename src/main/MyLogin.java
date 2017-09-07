d7c743e924e070551cd456e35e31fe7179ca7a59
wanzhs <wanzhs_cedar@163.com>
2017-09-07 19:18:30 +0800

    O Jesus, Your love is so strong 1

---
+++ src/main/MyLogin.java
@@ -0,0 +1,17 @@
+package main;
+
+public class MyLogin {
+
+	public static void main(String[] args) {
+		int i=0;
+		// TODO Auto-generated method stub
+		System.out.println("南燕姐姐最近好奇怪 ！ "+ i++);
+		System.out.println("南燕姐姐最近好奇怪 ！ "+ i++);
+		System.out.println("南燕姐姐最近好奇怪 ！ "+ i++);
+		System.out.println("南燕姐姐最近好奇怪 ！ "+ i++);
+		System.out.println("南燕姐姐最近好奇怪 ！ "+ i++);
+		System.out.println("南燕姐姐最近好奇怪 ！ "+ i++);
+
+	}
+
+}
